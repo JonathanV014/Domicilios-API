2c30b1197960df272f2206ceb2650338fa196a28
Jonathan Enrrique Vizcaino Macias <jonayma0110@gmail.com>
2025-04-28 18:04:12 -0500

    Feat: Creacion de direcciones con ciudades de colombia

--- asignacion_servicios/management/commands/generate_data.py
+++ asignacion_servicios/management/commands/generate_data.py
@@ -1,61 +1,41 @@
+import random
 from django.core.management.base import BaseCommand
 from faker import Faker
-from asignacion_servicios.models import Client, Address, Driver, Service
+from asignacion_servicios.models import Address, Driver
+
+COLOMBIAN_CITIES = [
+    "Bogotá", "Medellín", "Cali", "Barranquilla", "Cartagena", "Cúcuta", "Bucaramanga",
+    "Pereira", "Santa Marta", "Ibagué", "Villavicencio", "Manizales", "Neiva", "Pasto",
+    "Armenia", "Montería", "Sincelejo", "Popayán", "Valledupar", "Tunja"
+]
 
 class Command(BaseCommand):
-    help = 'Genera datos ficticios de Clients, Drivers y Services.'
+    help = 'Generar datos ficticios de Drivers y Services.'
 
     def handle(self, *args, **options):
         print("Generando datos...")
-        
-        self.generate_clients_with_addresses(n=50)
-
-
         self.generate_drivers_with_addresses(n=30)
-
         print("¡Datos generados con éxito!")
 
     def generate_address(self, faker):
+        city = random.choice(COLOMBIAN_CITIES)
         return Address.objects.create(
             name=faker.company(),
             country="Colombia",
-            city=faker.city(),  
+            city=city,
             street=faker.street_name(),
             latitude=faker.local_latlng(country_code="CO", coords_only=True)[0],
             longitude=faker.local_latlng(country_code="CO", coords_only=True)[1]
         )
 
-    def generate_clients_with_addresses(self, n: int = 10):
-        faker = Faker('es_CO')  
-
-        for _ in range(n):
-            address = self.generate_address(faker)  
-            client = Client.objects.create(
-                name=faker.name(),
-                phone=faker.phone_number(),
-                email=faker.email(),
-                address=address
-            )
-           
-            Service.objects.create(
-                pickup_address=address,
-                client=client,
-                status="pending"
-            )
-
-        print(f"{n} clientes y sus direcciones generados.")
-
     def generate_drivers_with_addresses(self, n: int = 10):
-
-        faker = Faker('es_CO')  
-
+        faker = Faker('es_CO')
         for _ in range(n):
-            address = self.generate_address(faker)  
+            address = self.generate_address(faker)
             Driver.objects.create(
                 name=faker.name(),
                 phone=faker.phone_number(),
                 address=address,
                 is_available=faker.boolean()
             )
-
         print(f"{n} conductores y sus direcciones generados.")
