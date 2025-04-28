89dc2eded64fefa22be165811904c1e11b535599
Jonathan Enrrique Vizcaino Macias <jonayma0110@gmail.com>
2025-04-27 19:35:47 -0500

    feat: Registro de los modelos para manipular facilmente desde /admin

--- asignacion_servicios/admin.py
+++ asignacion_servicios/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from asignacion_servicios.models import Driver, Service, Client, Address
+
+admin.site.register(Driver)
+admin.site.register(Service)
+admin.site.register(Client)
+admin.site.register(Address)
+
