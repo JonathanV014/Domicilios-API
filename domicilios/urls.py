06513fe8f1fad4772e15fa09e89dfe8f712f31fe
Jonathan Enrrique Vizcaino Macias <jonayma0110@gmail.com>
2025-04-27 19:41:57 -0500

    build: Configuración de los end-points para cada vista. Configuración de los enp-points para el manejo de tokens de JWT

--- domicilios/urls.py
+++ domicilios/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path, include
 from django.urls import path
+from asignacion_servicios.views import AddressViewSet, DriverViewSet, ClientViewSet, ServiceViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'addresses', AddressViewSet, basename='addresses')
+router.register(r'clients', ClientViewSet, basename='clients')
+router.register(r'drivers', DriverViewSet, basename='drivers')
+router.register(r'services', ServiceViewSet, basename='services')
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/', include(router.urls))
 ]
